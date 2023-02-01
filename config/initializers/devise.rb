5c8f6e797ac31be6a4aab04024464a8054d20d2c
Robi <robi.maliqi@hotmail.co.uk>
2023-02-01 20:27:36 +0000

    configure devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,6 +11,8 @@
 Devise.setup do |config|
 
   config.navigational_formats = ['*/*', :html, :turbo_stream]
+
+  config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET']
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
