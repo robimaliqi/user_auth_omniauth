6e898e9977e28850698322dfb4f2c90a658c24e0
Robi <robi.maliqi@hotmail.co.uk>
2023-02-01 20:14:16 +0000

    added controllers to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+ }
   get "/admin" => "pages#admin", as: "admin"
 
   root "pages#home"
